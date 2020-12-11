9126974a5b6260cb4de56f847886fae92a0c7cfc
monferon <sidelnikov.ns@gmail.com>
2020-12-11 21:02:39 +0300

    upd

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -2,7 +2,16 @@ import React from "react";
 import s from "./Calendar.module.css";
 
 const Calendar = () => {
-  return <div className={s.calendar}>CALENDAR</div>;
+  return (
+    <div className={s.calendar}>
+      <div className={s.calendarHeader}>
+        <button className={s.left}> LEFT</button>
+        <p className={s.central}> 11.12.2020</p>
+        <button className={s.right}>RIGHT</button>
+      </div>
+      <div>FORM</div>
+    </div>
+  );
 };
 
 export default Calendar;
